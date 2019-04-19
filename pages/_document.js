514283dcbc2ca399be7b2535a58f70197d5086d6
Bert Curtis <bertcurtis@berts-mbp.lan>
2019-04-19 02:36:42 -0600

    fixing video shiz

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
+  
 
   render () {
     return (
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.prepare()
     return handle(req, res)
   })
 
+
   server.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
