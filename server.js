ddd0589b37fc0e2ab9b4adbdbd753e3f45043eec
Bert Curtis <bert.curtis@divvypay.com>
2020-11-12 13:36:08 -0700

    re-routing server

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ app.prepare()
   server.get('*', (req, res) => {
     return handle(req, res)
   })
-
-  server.get('/inventory', (req, res) => {
-    return handle(req, res)
-  })
-
-
   server.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
